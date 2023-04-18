d48bb35c1ca4ffd31c9756489a64b93bd47b29e5
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-19 00:18:56 +0500

    patch method

--- book/src/app/api/orders/[orderId]/route.ts
+++ book/src/app/api/orders/[orderId]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
   { params }: { params: { orderId: string } }
 ) {
   const orderId = params.orderId;
-  console.log(orderId);
+
   //@ts-ignore
   const sql = postgres(process.env.DATABASE_URL, {
     ssl: require,
@@ -39,3 +39,48 @@ export async function DELETE(
   );
     return NextResponse.json({ success: "Order Deleted" }, { status: 201 });
 }
+
+
+
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { orderId: string } }
+) {
+  const orderId = params.orderId;
+  
+  let token = await request.headers.get("authtoken");
+ 
+  try {
+    let { clientName} = await request.json();
+    if (
+      clientName != null &&
+      clientName != "" 
+    ) {
+      // @ts-ignore
+      const sql = postgres(process.env.DATABASE_URL, {
+        ssl: require,
+      });
+
+      const orders = await sql.unsafe(
+        `select * from orders where createdby='${token}' and id='${orderId}'`
+      );
+      if (orders.length == 0) {
+        return NextResponse.json({ error: "No Order Found" }, { status: 404 });
+      } else {
+        const res = await sql.unsafe(
+          `UPDATE orders SET customername = '${clientName}' WHERE createdby='${token}' and id='${orderId}' `
+        );
+        return NextResponse.json({ success: "order updated" });
+      }
+    } else {
+      return NextResponse.json(
+        { error: "ClientName is not Provided" },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "Body Isn't Provided" }, { status: 500 });
+  }
+}
+
